76d99dccd0fc419f9a8f73e58bfcc4a27482de78
Chinmay Pandya <chinmaypandya043@gmail.com>
2024-05-13 15:51:35 +0530

    cors check

--- client/src/lib/actions.js
+++ client/src/lib/actions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseUrl = "http://localhost:5000/api";
+export const baseUrl = "https://chatappserver-chinmay.vercel.app/api";
 
 export const postReq = async (url,body) => {
     const response = await axios.post(url,body);
