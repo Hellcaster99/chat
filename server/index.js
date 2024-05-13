5dcb1f257b3f3453a7552b4e582848284fbb5cb7
Chinmay Pandya <chinmaypandya043@gmail.com>
2024-05-13 16:05:20 +0530

    cors check

--- server/index.js
+++ server/index.js
@@ -6,21 +6,24 @@ const chatRoute = require("./Routes/chatRoutes");
 const messageRoute = require("./Routes/messageRoutes");
 
 const app = express();
-require('dotenv').config()
+require('dotenv').config();
 
 app.use(express.json());
+app.use(express.urlencoded());
 app.use(cors());
 app.use('/api/users',userRoute);
 app.use('/api/chats',chatRoute);
 app.use('/api/messages',messageRoute);
 
 const PORT = 5000;
-const uri = process.env.MONGODB_URI
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri).then(()=>console.log("Connect to Mongo DB"))
+.catch((err)=>console.log(err));
 
 app.listen(PORT, (req,res)=>{
     console.log(`Server running on port ${5000}`);
-})
+});
 
-mongoose.connect(uri).then(()=>console.log("Connect to Mongo DB"))
-.catch((err)=>console.log(err))
+
 
